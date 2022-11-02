77b4c7048851c678b95482e09365ad25725adb6e
hazylua <40061395+hazylua@users.noreply.github.com>
2022-11-02 17:45:28 -0300

    Atualiza dados env

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,30 @@
+import crypto from 'crypto';
 import admin from 'firebase-admin';
 
-import serviceAccount from './serviceAccount.json';
+import encryptedServiceAccount from './serviceAccount.json';
 
 if (!admin.apps.length) {
+  const algorithm = 'aes-128-cbc';
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    process.env.SERVICE_ENCRYPTION_KEY,
+    process.env.SERVICE_ENCRYPTION_IV,
+  );
+  let decrypted = decipher.update(
+    encryptedServiceAccount.encrypted,
+    'base64',
+    'utf8',
+  );
+  decrypted += decipher.final('utf8');
+  const serviceAccount = JSON.parse(decrypted);
+
   try {
-    console.log('Firebase connect');
+    console.log('Firebase connect.');
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
   } catch (error) {
-    console.log('Firebase admin initialization error', error.stack);
+    console.log('Firebase admin initialization error.', error.stack);
   }
 }
 export default admin.firestore();
